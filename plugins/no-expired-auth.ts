6695d113036d3ce757ac1fc517b85b51cf4dd3b8
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2023-08-11 19:42:05 +0100

    feat: implement utility plugins & middlewares (route progress, expired auth clear)

---
+++ plugins/no-expired-auth.ts
@@ -0,0 +1,15 @@
+import JwtDecode from 'jwt-decode';
+
+export default defineNuxtPlugin(() => {
+  addRouteMiddleware(
+    'no-expired-auth',
+    () => {
+      const userStore = useUserStore();
+      if (userStore.accessToken) {
+        const decodedToken = JwtDecode<{ exp: number }>(userStore.accessToken as string);
+        if (decodedToken?.exp < Date.now() / 1000) userStore.clearUserSession();
+      }
+    },
+    { global: true },
+  );
+});
