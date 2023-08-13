bb7255a24753da12501559f140b8883670ed8cce
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2023-08-13 17:49:10 +0100

    feat: set-up entities admin routes

---
+++ pages/admin/lunch-groups/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { DataTable } from '@chronicstone/vue-sweettools';
+</script>
+
+<template>
+  <div>
+    <!-- <DataTable v-bind="lunchGroup"> Lunch groups </DataTable> -->
+  </div>
+</template>
