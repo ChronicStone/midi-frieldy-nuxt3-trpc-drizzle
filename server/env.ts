4bfe9a0bf98f3eeed56164a6ad93f42edb35e42f
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2023-08-18 13:20:08 +0200

    chore: add AWS env variables to server env

--- server/env.ts
+++ server/env.ts
@@ -8,5 +8,12 @@ export const env = createEnv({
     JWT_EXPIRATION_TIME: z.union([z.string(), z.number()]),
     CLIENT_APP_URL: z.string().url(),
     VITE_GOOGLE_API_KEY: z.string(),
+    AWS_ACCESS_KEY_ID: z.string(),
+    AWS_SECRET_ACCESS_KEY: z.string(),
+    AWS_SES_SOURCE_EMAIL: z.string(),
+    AWS_SES_REGION: z.string(),
+    AWS_REGION: z.string(),
+    AWS_S3_BUCKET: z.string(),
+    AWS_S3_URL: z.string(),
   },
 });
