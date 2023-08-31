9e0e94577f7385a2085fd9a358867d5e4a091c4d
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2023-08-31 17:06:44 +0400

    feat: extend useRemoteData composable to support default value + type inferrence

--- composables/useRemoteData.ts
+++ composables/useRemoteData.ts
@@ -1,16 +1,21 @@
 import { WatchSource } from 'vue';
 
-export function useRemoteData<T extends (...args: any[]) => Promise<any>>(
+export function useRemoteData<
+  T extends (...args: any[]) => Promise<any>,
+  Default extends Awaited<ReturnType<T>> | undefined,
+  Data = Default extends undefined ? Awaited<ReturnType<T>> | null : Awaited<ReturnType<T>>,
+>(
   resolver: T,
   options?: {
     watch?: WatchSource[];
     immediate?: boolean;
     deepWatch?: boolean;
+    default?: Default;
     onError?: <T>(err: T) => any;
   },
 ) {
-  const data = ref<Awaited<ReturnType<T>> | null>(null);
-  const pending = ref(false);
+  const data = ref<Data>((options?.default ?? null) as unknown as Awaited<ReturnType<T>>);
+  const pending = ref<boolean>(false);
 
   async function refresh() {
     try {
